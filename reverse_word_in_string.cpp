73b62eb5b9d37507b77344984ebc2726c7ceaa0e
Mohit9616 <75412459+Mohit9616@users.noreply.github.com>
2021-09-26 01:52:30 +0530

    Add files via upload

---
+++ reverse_word_in_string.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	string s,res=" ";
+	// cout<<res;
+	getline(cin,s);
+	int n=s.size();
+	int i=0,j=0;
+	while(i<n){
+		while(i<0 && s[i]==' ')
+			i++;
+		j=i;
+		while(j<n && s[j]!=' ')
+			j++;
+		string w=s.substr(i,j-i);
+		if(res=="  ")
+			res=w;
+		else
+			res=w+" "+res;
+		i=j+1;
+	}
+	cout<<res<<endl;
+}
